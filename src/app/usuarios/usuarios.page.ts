f52dba5a2df156dbd1bf1549fd6493448097166a
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-01-05 22:32:36 -0300

    criando mecanismo de busca por nome ou cpf

--- src/app/usuarios/usuarios.page.ts
+++ src/app/usuarios/usuarios.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+//import { resolve } from 'dns';
 import { Api } from 'src/services/api';
 
 @Component({
@@ -40,6 +41,36 @@ getData() {
 
     });
 
+  }
+
+  addUsuarios(){
+    this.router.navigate(['add-usuario']);
+  }
+
+  carregar(){
+    return new Promise(resolve => {
+      this.clientes = [];
+
+      let dados = {
+        requisicao : 'listar',
+        nome : this.nome,
+      };
+
+        this.provider.dadosApi(dados, 'http://localhost/api-ionic/index.php?q=listar').subscribe((data:any) => {
+
+        if(data['status'] == 200)
+        {
+          let a = data.dados;
+
+          this.clientes = [];
+          for(let i in a)
+          {
+            this.clientes.push(a[i])
+          }
+        }
+
+      });
+    });
   }
 
 }
